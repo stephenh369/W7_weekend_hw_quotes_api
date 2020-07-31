30329b0606dab61409de8de939a1acb274b04939
Stephen <stephen_h369@live.co.uk>
2020-07-31 20:23:28 +0100

    more css, box shadow and non functional button added

--- src/components/ProgrammingQuote.vue
+++ src/components/ProgrammingQuote.vue
@@ -2,7 +2,10 @@
   <div class="quote-div">
     <p>{{randomQuote.en}}</p>
     <br>
-    <p class="quote-author">{{randomQuote.author}}</p>
+    <div class="flex-div">
+        <p class="quote-author">{{randomQuote.author}}</p>
+        <button class="btn">Another Quote!</button>
+    </div>
   </div>
 </template>
 
@@ -28,8 +31,28 @@ export default {
         opacity: 0.9;
         padding: 0.5rem;
         border-radius: 10px;
+        text-align: center;
+        box-shadow: 2px 2px 5px 5px rgb(50,64,93,0.5);
     }
     .quote-author {
+        color: #0466C8;
+    }
+    .flex-div {
+        display: flex;
+        height: 50%;
+        align-items: center;
+        justify-content: space-around;
+    }
+    .btn {
+        background-color: #242E42;
+        color: #D2D4DB;
+        cursor: pointer;
+        border: none;
+        border-bottom: 1px #0353A4 solid;
+        border-radius: 5px;
+        padding: 5px;
+    }
+    .btn:hover {
         color: #0466C8;
     }
 </style>
